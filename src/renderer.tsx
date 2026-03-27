3fafde253ba237509c6f978b727e600a1661f640
festival233 <hehspace@gmail.com>
2026-03-27 18:30:56 +0000

    Add Google Analytics gtag snippet to page head

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,6 +4,13 @@ export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZGFLP5LT1H"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZGFLP5LT1H');
+        ` }} />
         <link href="/static/style.css" rel="stylesheet" />
       </head>
       <body>{children}</body>
